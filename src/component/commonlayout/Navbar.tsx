83aab44379978587c334f370199fb4583a4cd707
Rishabh <guptarishabh792@gmail.com>
2024-08-23 15:39:42 +0530

    setup project

---
+++ src/component/commonlayout/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import NavbarItems from "./Items";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-black text-white py-4 shadow-[0_4px_6px_rgb(0,0,0,0.1)] z-50">
+      <div className="flex justify-between md:grid md:grid-cols-3 lg:flex lg:justify-between items-center px-2 md:px-4 lg:px-24">
+        <Link
+          href={"/"}
+          aria-label="Home"
+          className="mx-auto w-fit lg:w-auto lg:mx-0"
+        >a</Link>
+        <NavbarItems />
+        <div className="flex justify-end">
+          <Link href="/login" aria-label="login page">a</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/data/tabs.ts
@@ -0,0 +1,17 @@
+export const tabs = [
+  { id: 1, label: "Home", path: "/" },
+  { id: 2, label: "Services", path: "/services" },
+  { id: 4, label: "Blogs", path: "/blog" },
+  { id: 3, label: "About Us", path: "/about" },
+  { id: 5, label: "Contact Us", path: "/contact-us" },
+];
+
+export const footerTabs = [
+  { id: 1, label: "Privacy Policy", path: "/privacy-policy" },
+  {
+    id: 2,
+    children: [],
+    label: "Terms And Conditions",
+    path: "/terms-and-conditions",
+  },
+];
