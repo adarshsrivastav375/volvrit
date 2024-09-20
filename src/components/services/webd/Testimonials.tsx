f07200fa09f41ead75ce7cf67061a85d1253817c
Rishabh gupta <guptarishabh79@gmail.com>
2024-09-20 17:26:58 +0530

    make homepage

--- src/components/services/webd/Testimonials.tsx
+++ src/components/services/webd/Testimonials.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 const Testimonials = () => {
   return (
-    <div className="container mx-auto px-4 md:px-6 lg:px-20 text-center pb-8 md:pb-12 lg:pb-16">
+    <div className="container mx-auto px-4 md:px-6 lg:px-20 text-center py-8 md:py-12 lg:py-16">
       <HeadingText
         headingText2="Our Projects"
         colored="text-primary font-bold"
         headingText1="Work Speaks Volumes: Discover"
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 pt-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 pt-5 md:pt-10">
         <div className="flex flex-col gap-5 md:gap-10 h-fit">
           <Image
             width={800}
