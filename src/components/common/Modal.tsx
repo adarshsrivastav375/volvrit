ec6c2394dc03b33b8bfda71cc6ab74ba562c66cc
Rishabh gupta <guptarishabh79@gmail.com>
2024-10-07 16:21:47 +0530

    fix

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -39,7 +39,7 @@ const Modal: React.FC<ModalProps> = ({
               />
             </p>
           </div>
-          <div>{children}</div>
+          <div className="-mt-10">{children}</div>
         </div>
       </div>
     </div>,
