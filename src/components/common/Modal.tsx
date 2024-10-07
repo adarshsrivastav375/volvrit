5100ea65df50ce0e3deec9e8e40e055ccf19208c
Rishabh gupta <guptarishabh79@gmail.com>
2024-10-07 16:20:50 +0530

    fix code

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -23,7 +23,7 @@ const Modal: React.FC<ModalProps> = ({
       <div className="fixed inset-0 bg-white backdrop-blur-lg bg-opacity-10"></div>
       <div className="bg-opacity-50 backdrop-filter backdrop-blur-lg shadow-lg rounded-xl overflow-hidden w-fit z-10 relative">
         <div className="bg-white overflow-scroll no-scrollbar max-h-[80vh] md:max-h-[90vh]">
-          <div className="flex justify-between items-center p-4">
+          <div className="flex fixed top-0 bg-white w-full justify-between items-center p-4">
             {showLogo && (
               <span
                 className={`text-xl md:text-base lg:text-xl 2xl:text-3xl 3xl:text-4xl 4xl:text-6xl 5xl:text-7xluppercase font-semibold`}
--- src/components/common/modals/CalenderModal.tsx
+++ src/components/common/modals/CalenderModal.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Modal from "../Modal";
 
 const CalenderModal = ({
@@ -11,11 +12,12 @@ const CalenderModal = ({
     setIsVisible(false);
   };
 
+
   return (
     <Modal isVisible={isVisible} onClose={handleclose} showLogo={true}>
       <div
         style={{ height: "542px", width: "100%;" }}
-        className="w-full md:min-w-[524px] lg:min-w-[1024px] 2xl:min-w-[1280px] bg-black"
+        className="w-full md:min-w-[524px] lg:min-w-[1024px] xl:min-w-[1096px] 2xl:min-w-[1380px] bg-black"
       >
         <iframe
           src="https://cal.com/volvrit/start-your-project-with-volvrit"
